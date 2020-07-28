807b10882bedb6437c3709196eb49b9fc588751a
communal-documentation-0347 <scottkoons@gmail.com>
2020-07-28 21:54:28 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -8,8 +8,8 @@ var playlist = {
 // }
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[artistName] = songTitle
-  return playlist
+  playlist[artistName] = songTitle;
+  return playlist;
 }
 
 function removeFromPlaylist(playlist, artistName) {
