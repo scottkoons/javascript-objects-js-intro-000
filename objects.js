7fab53ed4b7834535b36721763cc30f0112e5fa5
communal-documentation-0347 <scottkoons@gmail.com>
2020-07-28 21:54:21 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -11,3 +11,8 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
   return playlist
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist."Slowdive";
+  return playlist;
+}
